be42754c8f6ab97940ae67ffa3f368b7f3ebab2c
Sadik-Ahmmed-Tonmoy <100148160+Sadik-Ahmmed-Tonmoy@users.noreply.github.com>
2023-09-14 12:51:46 +0600

    slider

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import CategorySlider from '../CategorySlider/CategorySlider';
 
 const Home = () => {
     return (
         <div>
-            
+        <CategorySlider/>
         </div>
     );
 };
