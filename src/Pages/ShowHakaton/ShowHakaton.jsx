88e31da834bf5ef4433a385550e8f76f8ad69d98
NabilaWorld <mnabila30163@gmail.com>
2023-09-14 14:27:55 +0600

    commit the details

--- src/Pages/ShowHakaton/ShowHakaton.jsx
+++ src/Pages/ShowHakaton/ShowHakaton.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import './ShowHakaton.css'
+
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 
 const ShowHakaton = () => {
     // Assuming that useLoaderData returns an object with a 'title' property
-    const { title } = useLoaderData();
+    const { title, description, banner, publisher, first_prize, second_prize, third_prize, category } = useLoaderData();
+
+    useEffect(() => {
+        Aos.init({
+            offset: 200,
+            easing: 'ease-in-sine',
+            duration: 600
+        });
+    }, []);
 
     return (
         <div>
-            <h1>{title}</h1>
+            <center >
+                <h1 className='text-center mt-10 text-4xl'>{title}</h1>
+
+                <p className='text-center mt-5 text-2xl'>{category}</p>
+
+                <img className='my-10 w-full' src={banner} alt="" />
+
+                <div data-aos="fade-up" data-aos-offset="200" data-aos-duration="2000" >
+                    <p className='text-xl'>{description}</p>
+
+                    <p className='text-2xl mt-3'>Publisher:{publisher.name}</p>
+
+                    {/* <p className='text-2xl mt-3'>{publisher.logo}</p> */} <br />
+
+                    <p className='text-2xl mt-3'> <span className='text-3xl font-bold'>First Price:</span> {first_prize} </p>
+
+                    <p className='text-2xl mt-3'> <span className='text-3xl font-bold'></span> {second_prize}</p>
+
+                    <p className='text-2xl mt-3'> <span className='text-3xl font-bold'></span> {third_prize}</p>
+                </div>
+
+                <Link to="/"><button className='btn btn-warnings my-10 text-white'>Back</button></Link>
+
+            </center>
         </div>
     );
 };
